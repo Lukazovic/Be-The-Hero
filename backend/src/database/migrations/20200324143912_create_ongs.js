700d0609e930cfed872ea3577225dae007fd9495
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-03-24 14:57:00 -0300

    Configuring database

--- backend/src/database/migrations/20200324143912_create_ongs.js
+++ backend/src/database/migrations/20200324143912_create_ongs.js
@@ -1,11 +1,11 @@
 exports.up = function(knex) {
   return knex.schema.createTable('ongs', function(table) {
-    table.String('id').primary();
-    table.String('name').notNullable();
-    table.String('email').notNullable();
-    table.String('whatsapp').notNullable();
-    table.String('city').notNullable();
-    table.String('uf', 2).notNullable();
+    table.string('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('whatsapp').notNullable();
+    table.string('city').notNullable();
+    table.string('uf', 2).notNullable();
   });
 };
 
