a6933679dbb6375cb227f3677c6600d77a632c32
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-03-24 14:07:35 -0300

    Configuring routes file

---
+++ backend/src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/', (request, response) => {
+  return response.json({
+    message: 'Hello World'
+  });
+});
+
+module.exports = routes;
