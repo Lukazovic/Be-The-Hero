bf6ee96508cfc584415b350575cc50e915f17a64
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-03-25 13:15:29 -0300

    Add New incident page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import { FiPower , FiTrash2} from 'react-icons/fi';
 
-import './style.css'
+import './styles.css'
 
 import logoImg from '../../assets/logo.svg';
 
