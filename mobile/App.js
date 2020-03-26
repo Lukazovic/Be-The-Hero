e942f6e8f678fc6b470671d18366d7819c229f95
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-03-26 17:50:54 -0300

    Add images

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -12,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#715c91',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
